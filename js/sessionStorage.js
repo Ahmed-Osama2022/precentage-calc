a356da15388c30cb8680a8ea46de381e90c3b172
ahmed-osama2022 <ahmadosama.2011@gmail.com>
2023-06-12 13:00:09 +0300

    Adding a new fature "sessionStorage" && "Enter key"

--- js/sessionStorage.js
+++ js/sessionStorage.js
@@ -5,12 +5,28 @@
 
 import { maximumInput, currentLevel } from "./testFinal.js";
 
-sessionStorage.getItem('value');
+let savedNumber = sessionStorage.getItem('value');
+maximumInput.value = savedNumber;
 
 window.onload = () => {
     maximumInput.focus();
 };
+
 maximumInput.onblur = () => {
+    console.log(maximumInput.value);
+    
     sessionStorage.setItem('value', maximumInput.value);
     maximumInput.nextElementSibling.focus();
 };
+
+// Add enter key feature when finishes required input values
+currentLevel.addEventListener('keyup', (e) => {
+    // To get the key name ...
+    // console.log(e); 
+    // ||
+    // console.log(e.key);
+    if (e.key === 'Enter') {
+        currentLevel.nextElementSibling.click();
+        console.log('ahmed');
+    };
+});
